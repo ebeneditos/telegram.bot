2e0815cf6c9984a033b665b26bd7bdb153c08e7b
Ernest Benedito <ebeneditos@gmail.com>
2018-01-29 21:26:45 +0100

    Add appveyor and tests update

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -1,13 +1,23 @@
 
-# Provide public token so it's easy for contributors to run tests on their local machine
-token <- "532598148:AAEtazTsFqOXznfxWFaslPru-oZCI7EKR0E" # username: @RTelegramExtBot
+# You must define a valid TOKEN in Travis Environment Variables Settings
+# NOTE: https://docs.travis-ci.com/user/environment-variables/#Defining-Variables-in-Repository-Settings
+token <- Sys.getenv('TOKEN')
 
+# Check if the token is valid, otherwise set a default token
+# NOTE: This will skip the connection tests, as this token is not valid
+res <- try(Bot(token = token))
+if (inherits(res, 'try-error') && attr(t, 'condition')$message == "invalid token.")
+  token <- "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11"
+
+# Skip if it is run offline or the bot request is not valid
 skip_if_offline <- function(bot){
   
   res <- try(bot$getMe())
   
   if (inherits(res, 'try-error'))
     skip(attr(res, 'condition')$message)
+  else if (inherits(res, 'response') && res$status_code != 200)
+    skip('Bad request.')
 }
 
 # Handler function
@@ -22,8 +32,8 @@ foo_update <- list(update_id = 0,
 foo_callbackquery <- list(update_id = 1,
                           callback_query = list(data = 'foo'))
 
-# Foo bot 
-# only used for start_polling testing, as the bot features are tested in other contexts
+# Foo bot and foo updater
+# NOTE: Only used for start_polling testing, as the bot features are tested in other contexts
 foo_bot <- list(
   clean_updates = function(...){}
 )
