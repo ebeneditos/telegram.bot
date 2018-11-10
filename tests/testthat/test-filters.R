b2d1110f9f272196cc4a3bd26b96f8ec7d8e2e08
Ernest Benedito <ebeneditos@gmail.com>
2018-11-10 22:29:07 +0100

    Documentation update

--- tests/testthat/test-filters.R
+++ tests/testthat/test-filters.R
@@ -1,42 +1,42 @@
 
-context('Filters')
+context('MessageFilters')
 
 message <- Update(foo_update)$message
 
 test_that("Filter Message", {
   
-  expect_true(Filters$all(message))
+  expect_true(MessageFilters$all(message))
   
-  expect_false(Filters$text(message))
+  expect_false(MessageFilters$text(message))
   
-  expect_true(Filters$command(message))
+  expect_true(MessageFilters$command(message))
   
-  expect_false(Filters$reply(message))
+  expect_false(MessageFilters$reply(message))
   
-  expect_false(Filters$audio(message))
+  expect_false(MessageFilters$audio(message))
   
-  expect_false(Filters$document(message))
+  expect_false(MessageFilters$document(message))
   
-  expect_false(Filters$photo(message))
+  expect_false(MessageFilters$photo(message))
   
-  expect_false(Filters$sticker(message))
+  expect_false(MessageFilters$sticker(message))
   
-  expect_false(Filters$video(message))
+  expect_false(MessageFilters$video(message))
   
-  expect_false(Filters$voice(message))
+  expect_false(MessageFilters$voice(message))
   
-  expect_false(Filters$contact(message))
+  expect_false(MessageFilters$contact(message))
   
-  expect_false(Filters$location(message))
+  expect_false(MessageFilters$location(message))
   
-  expect_false(Filters$venue(message))
+  expect_false(MessageFilters$venue(message))
   
-  expect_false(Filters$forwarded(message))
+  expect_false(MessageFilters$forwarded(message))
   
-  expect_false(Filters$game(message))
+  expect_false(MessageFilters$game(message))
   
-  expect_false(Filters$invoice(message))
+  expect_false(MessageFilters$invoice(message))
   
-  expect_false(Filters$successful_payment(message))
+  expect_false(MessageFilters$successful_payment(message))
   
 })
